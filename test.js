e4b8eb801f4aa18338e3c914f6b3d3906397acf0
hanssenstijn <stijn.hanssen@accenture.com>
2020-07-23 13:53:27 +0200

    test cases

--- test.js
+++ test.js
@@ -7,3 +7,8 @@ expect(user).to.have.property('name')
 
 exprect({}).to.exist;
 expect(26).to.equal(26);
+expect(false).to.be.false;
+expect('hello').to.be.string;
+expect([1, 2, 3]).to.not.be.empty;
+expect([1, 2, 3]).to.have.length.of.at.least(3);
+expect([1, 2]).to.be.an('array').that.does.not.include(3);
