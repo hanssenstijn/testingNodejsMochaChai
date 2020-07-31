c13783c1dbe47d13e7fd51188e80a544045d98df
hanssenstijn <stijn.hanssen@accenture.com>
2020-07-31 12:35:13 +0200

    stub example

---
+++ moduleUnderTest.js
@@ -0,0 +1,10 @@
+const dependencyModule = require("./dependencyModule");
+
+function getTheSecret() {
+  return `The secret was: ${dependencyModule.getSecretNumber()}`;
+}
+
+module.exports = {
+  getTheSecret
+};
+
